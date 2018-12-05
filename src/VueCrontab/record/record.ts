bf4489d22e2e39e7b9d9f8a6ee0d1817457d2386
hirasaki1985 <hirasaki1985@gmail.com>
2018-12-05 17:55:00 +0900

    implemented the record and recordTest.

--- src/VueCrontab/record/record.ts
+++ src/VueCrontab/record/record.ts
@@ -1,31 +1,81 @@
+/**
+ * Class that records the execution result of VueCrontab.
+ *
+ */
 export default class VueCrontabRecord {
-  public counter: number
   private results: Array<object>
-  public last_run: Date
+  public max_rec_num: number
 
-  constructor () {
-    this.counter = 0
-    this.results = []
-    this.last_run = null
+  /**
+   * constructor with option.
+   * @param {Object} option
+   * @param {String} [options.max_rec_num=1] Maximum number of records to record execution results.
+   */
+  constructor (option: Object = {}) {
+    this.initialize(option)
   }
 
-  addResult (match_date: Date, result: Object): Boolean {
+  /**
+   * initialize
+   * @param {Object} option
+   */
+  initialize(option: Object = {}) {
+    this.results = []
+    this.max_rec_num = option['max_rec_num'] || 1
+  }
+
+  /**
+   * Add the execution result of VueCrontab.
+   * @param {Date} match_date execution time of VueCrontab.
+   * @param {any} result result execution of VueCrontab.
+   * @return {number} return new length of array.
+   */
+  addResult (match_date: Date, result: any): number {
+    if (this.results.length >= this.max_rec_num) {
+      this.deleteFirstResult()
+    }
+    return this.pushResult(match_date, result)
+  }
+
+  /**
+   * Delete the first execution result.
+   * @return {Object} delete execution.
+   */
+  private deleteFirstResult (): object {
+    return this.results.shift()
+  }
+
+  /**
+   *
+   * @param {Date} match_date execution time of VueCrontab.
+   * @param {any} result result execution of VueCrontab.
+   * @return {number} return new length of array.
+   */
+  private pushResult (match_date: Date, result: any): number {
     const add_result = {
       match_date: match_date,
       result: result,
       finish_date: new Date()
     }
-    this.results.push(add_result)
-    this.counter++
-    this.last_run = match_date
-    return true
+    return this.results.push(add_result)
   }
 
+  /**
+   * Return the execution latest result of crontab
+   */
   getLastResult() {
     if (this.results.length === 0) {
       return {}
     }
     return this.results[this.results.length -1]
+  }
+
+  /**
+   * Returns the execution result of crontab
+   * @return Array<Object>
+   */
+  getResults(): Array<object> {
+    return this.results
   }
 }
 
---
+++ test/unit/spec/VueCrontab/record/record_test.spec.ts
@@ -0,0 +1,56 @@
+import VueCrontabRecord from '../../../../../src/VueCrontab/record/record'
+
+describe('crontab test', () => {
+  // isMatch
+  it('default option', () => {
+    console.log('## record_test. default option.')
+    let record = new VueCrontabRecord()
+    console.log(record)
+
+    // option check
+    expect(record.max_rec_num).toEqual(1)
+  })
+
+  it('addResult', () => {
+    console.log('## record_test. addResult.')
+    const options: Array<Object> = [
+      {},
+      {max_rec_num: 5},
+      {max_rec_num: 10}
+    ]
+    for (let i in options) {
+      let option = options[i]
+      console.log('test option')
+      console.log(option)
+      let record = new VueCrontabRecord(option)
+      const addResults: Array<any> = [
+        // match_date, job result
+        [new Date('2018-12-03T01:28:05'), 1],
+        [new Date('2018-12-03T01:28:10'), 2],
+        [new Date('2018-12-03T01:28:15'), 3],
+        [new Date('2018-12-03T01:28:20'), 4],
+        [new Date('2018-12-03T01:28:25'), 5],
+        [new Date('2018-12-03T01:28:30'), 6],
+        [new Date('2018-12-03T01:28:35'), 7],
+      ]
+
+      for (let j in addResults) {
+        console.log('addResult()')
+        let add_result = record.addResult(addResults[j][0], addResults[j][1])
+
+        let max_rec_num: number = Number(j) + 1
+        if (typeof(option['max_rec_num']) === 'number' && max_rec_num > option['max_rec_num']) {
+          max_rec_num = option['max_rec_num']
+          console.log(record.getResults())
+        } else if (typeof(option['max_rec_num']) === 'undefined') {
+          max_rec_num = 1
+        }
+        console.log(add_result)
+        console.log(max_rec_num)
+        expect(add_result).toEqual(max_rec_num)
+        console.log()
+      }
+      console.log(record.getResults())
+    }
+  })
+})
